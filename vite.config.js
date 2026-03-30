ed7c5142154dcc1c92316f12c1bfd22e4aad5b93
teamquipsdrips <teamquipsdrips@gmail.com>
2026-03-31 04:20:37 +0500

    Changed the base section in vite config.

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/atquantech/",
   plugins: [
     tailwindcss(),
     react()
   ],
   server: {
-    allowedHosts: ["imelda-philhellenic-blamably.ngrok-free.dev"],
-    base: "/atquantech/"
+    allowedHosts: ["imelda-philhellenic-blamably.ngrok-free.dev"]
   }
 })
